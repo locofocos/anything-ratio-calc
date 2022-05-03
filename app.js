c0b1e80348c76a662514c78e039757a27550faf9
Patrick Boatner <patrickboatner@gmail.com>
2022-05-02 21:48:52 -0500

    Coffee calculator kind of working

--- app.js
+++ app.js
@@ -2,19 +2,46 @@
 
 const App = () => {
   return <div>
-    <ExampleComponent title="example component title"/>
-    <p>It's working</p>
+    <Calculator/>
   </div>
 }
 
-class ExampleComponent extends React.Component {
-  constructor (props) {
-    super()
-  }
+const Calculator = () => {
 
-  render () {
-    return <h1>{this.props.title}</h1>
-  }
+  const [recipeCoffeeG, setRecipeCoffeeG] = React.useState(8)
+  const [recipeWaterOz, setRecipeWaterOz] = React.useState(6)
+
+  const [calculatedCoffeeG, setCalculatedCoffeeG] = React.useState(recipeCoffeeG)
+  const [calculatedWaterOz, setCalculatedWaterOz] = React.useState(recipeWaterOz)
+
+
+  return <div>
+    <h2>Recipe values</h2>
+    <div>
+      <span>Coffee (g)</span>
+      <input value={recipeCoffeeG} onChange={(e) => setRecipeCoffeeG(e.target.value)}/>
+    </div>
+    <div>
+      <span>Water (oz)</span>
+      <input value={recipeWaterOz} onChange={(e) => setRecipeWaterOz(e.target.value)}/>
+    </div>
+
+    <h2>Calculated values</h2>
+    <div>
+      <span>Coffee (g)</span>
+      <input value={calculatedCoffeeG} onChange={(e) => {
+        setCalculatedCoffeeG(e.target.value)
+        setCalculatedWaterOz(parseFloat(e.target.value) * parseFloat(recipeWaterOz) / parseFloat(recipeCoffeeG))
+      }}/>
+    </div>
+    <div>
+      <span>Water (oz)</span>
+      <input value={calculatedWaterOz} onChange={(e) => {
+        setCalculatedWaterOz(e.target.value)
+        setCalculatedCoffeeG(parseFloat(e.target.value) * parseFloat(recipeCoffeeG) / parseFloat(recipeWaterOz))
+      }}/>
+    </div>
+  </div>
 }
 
 const root = ReactDOM.createRoot(document.getElementById('app'))
